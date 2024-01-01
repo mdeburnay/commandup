e7cd428f788bb6fbdfb5cc64a5405e85fc892af7
Max De Burnay-Bastos <max.deburnay-bastos@calebandbrown.com>
2024-01-01 21:13:10 +1100

    sorted hello world func

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func HelloWorld() string {
-	return "Hello World"
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
