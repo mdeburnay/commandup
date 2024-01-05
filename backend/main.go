b9b4036ac60b7bcc0f0365130a80f8ed1e2a068e
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-01-05 14:50:22 +1100

    wip - hooking up front and backend

--- backend/main.go
+++ backend/main.go
@@ -6,22 +6,26 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"} // Change to frontend domain in PROD
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	r.Use(cors.New(config))
+
 	// Define a route to serve static files (e.g., your React frontend)
 	r.StaticFS("/static", http.Dir(filepath.Join(".", "frontend", "build", "static")))
 
-	// Define a route for card-related operations
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "Hello World")
 	})
 	r.GET("/api/cards/upgrades", handlers.GetCards)
 
-	// Run the server
 	port := ":8080"
 	fmt.Printf("Server is running on port %s\n", port)
 	r.Run(port)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,55 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import axios from "axios";
 
+// Create a client
+const queryClient = new QueryClient();
+
 function App() {
-  const [upgradeCards, setUpgradeCards] = useState([]);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CardUpgrades />
+    </QueryClientProvider>
+  );
+}
+
+function CardUpgrades(): JSX.Element {
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ["cardUpgrades"],
+    queryFn: () =>
+      axios({
+        method: "get",
+        url: "http://localhost:8080/api/cards/upgrades",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }).then((res) => {
+        return res.data;
+      }),
+  });
+
+  if (isLoading) return <div>"Loading..."</div>;
+
+  if (error) return <div>"An error has occurred: " + error.message</div>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {isFetching
+            ? "Fetching your cards..."
+            : data.map((card: string) => {
+                return <p>{card}</p>;
+              })}
         </p>
         <a
           className="App-link"
