e816f7c739f3d50730538e029db0000168c6189d
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-02-13 14:39:47 +1100

    aligned items instead of self centering

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,16 +1,8 @@
 export const Login = () => {
   return (
-    <div className="flex justify-center flex-col">
-      <input
-        className="flex w-80 p-1 self-center"
-        type="text"
-        placeholder="Email"
-      />
-      <input
-        className="flex w-80 p-1 self-center"
-        type="text"
-        placeholder="Password"
-      />
+    <div className="flex justify-center items-center flex-col">
+      <input className="flex w-80 p-1" type="text" placeholder="Email" />
+      <input className="flex w-80 p-1" type="text" placeholder="Password" />
     </div>
   );
 };
