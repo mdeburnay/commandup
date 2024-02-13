4ecc2537ff6adefca061a5ec672ea4d2ff12b2f2
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-02-13 14:41:51 +1100

    edited input components

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,16 @@
 export const Login = () => {
   return (
     <div className="flex justify-center items-center flex-col">
-      <input className="flex w-80 p-1" type="text" placeholder="Email" />
-      <input className="flex w-80 p-1" type="text" placeholder="Password" />
+      <input
+        className="flex w-80 p-1 rounded-md m-2"
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        className="flex w-80 p-1 rounded-md m-2"
+        type="text"
+        placeholder="Password"
+      />
     </div>
   );
 };
