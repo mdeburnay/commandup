46ec2df71e5a078d5454b81778f261f28c0146fc
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-02-13 12:02:49 +1100

    added react routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 // Dependencies
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Styles
@@ -17,7 +18,11 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Container>
-        <Home />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
       </Container>
     </QueryClientProvider>
   );
--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,9 +1,26 @@
 interface IButtonProps {
   text: string;
   onClick?: () => void;
+  type?: ButtonType;
 }
 
-export const Button = ({ text, onClick }: IButtonProps) => {
+enum ButtonType {
+  LOGIN = "login",
+  UPLOAD = "upload",
+}
+
+export const Button = ({ text, onClick, type }: IButtonProps) => {
+  if (type === ButtonType.LOGIN) {
+    return (
+      <button
+        className="w-auto px-6 py-1 bg-white text-blue rounded-lg tracking-wide border cursor-pointer"
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    );
+  }
+
   return (
     <button
       className="w-auto px-6 py-1 bg-white text-blue rounded-lg tracking-wide border cursor-pointer"
