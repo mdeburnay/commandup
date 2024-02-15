f14acc36f987ca4bbdff605d6d1f920d4babf239
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-02-15 12:12:45 +1100

    created create user function, installed uuid package

--- backend/models/users.go
+++ backend/models/users.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type User struct {
 	ID        int
 	Email     string
@@ -12,4 +18,9 @@ type User struct {
 func GetUser(email string) (user User, err error) {
 	err = db.QueryRow("SELECT id, email, password FROM users WHERE email = ?", email).Scan(&user.ID, &user.Email, &user.Password)
 	return user, err
+}
+
+func CreateUser(email string, password string) (err error) {
+	_, err = db.Exec("INSERT INTO users (id, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)", uuid.New(), email, password, time.Now(), time.Now())
+	return err
 }
