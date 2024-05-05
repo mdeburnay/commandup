342e67bc68fdbaaf5a95fa9f949f8a477ef3de24
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-05-05 19:13:41 +1000

    test commit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -75,6 +75,7 @@ function CardUpgrades(): JSX.Element {
             />
           </div>
           <div className="mx-5">
+            <div>{"Testing"}</div>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="deck-name"
