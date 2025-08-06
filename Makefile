4f8acef1d0208fa48608600445a5cad803a682ac
Max De Burnay-Bastos <max@Maxs-MacBook-Pro.local>
2025-08-07 09:25:07 +1000

    starting again

--- Makefile
+++ Makefile
@@ -6,14 +6,6 @@ up:
 	docker-compose up -d
 	@echo "Docker images started!"
 
-## UP BUILD: Stops docker containers (if running), builds all projects and starts docker compose
-up_build: build_broker build_auth
-	@echo "Stopping docker images (if running...)"
-	docker-compose down
-	@echo "Building (when required) and starting docker images..."
-	docker-compose up --build -d
-	@echo "Docker images built and started!"
-
 ## DOWN: Stop docker containers
 down:
 	@echo "Stopping docker compose..."
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,133 +10,133 @@ import { Button } from "../components/Buttons/PrimaryButton";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 interface ICard {
-  name: string;
-  synergy: number;
-  inclusion: number;
+	name: string;
+	synergy: number;
+	inclusion: number;
 }
 interface ICardCategory {
-  title: string;
-  cards: ICard[];
+	title: string;
+	cards: ICard[];
 }
 
 interface GetCardUpgradesReq {
-  commander: string;
-  precon?: string;
+	commander: string;
+	precon?: string;
 }
 
 export const Home = () => {
-  return (
-    <>
-      <CardUpgrades />
-    </>
-  );
+	return (
+		<>
+			<CardUpgrades />
+		</>
+	);
 };
 
 function CardUpgrades(): JSX.Element {
-  const [commander, setCommander] = useState("");
-  const [deckName, setDeckName] = useState("");
-  const [error, setError] = useState("");
+	const [commander, setCommander] = useState("");
+	const [deckName, setDeckName] = useState("");
+	const [error, setError] = useState("");
 
-  const {
-    mutate: fetchCardUpgrades,
-    isError,
-    isPending,
-    data,
-  } = useMutation({
-    mutationFn: async (payload: GetCardUpgradesReq) => {
-      return axios
-        .post("http://localhost:8080/api/cards/upgrades", payload, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-        .then(({ data }) => {
-          console.log("Data: ", data);
-          return data;
-        })
-        .catch(({ response }) => {
-          const errMessage = response.data.error;
+	const {
+		mutate: fetchCardUpgrades,
+		isError,
+		isPending,
+		data,
+	} = useMutation({
+		mutationFn: async (payload: GetCardUpgradesReq) => {
+			return axios
+				.post("http://localhost:8080/api/cards/upgrades", payload, {
+					headers: {
+						"Content-Type": "application/json",
+					},
+				})
+				.then(({ data }) => {
+					console.log("Data: ", data);
+					return data;
+				})
+				.catch(({ response }) => {
+					const errMessage = response.data.error;
 
-          console.log(errMessage);
-          setError(errMessage);
-        });
-    },
-  });
+					console.log(errMessage);
+					setError(errMessage);
+				});
+		},
+	});
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    const payload = {
-      commander: commander,
-      precon: deckName,
-    };
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+		const payload = {
+			commander: commander,
+			precon: deckName,
+		};
 
-    fetchCardUpgrades(payload);
-  };
+		fetchCardUpgrades(payload);
+	};
 
-  /**
-   * TODO:
-   * 1. Create a form with two inputs: commander and deck name
-   * 2. Create a button to submit the form
-   * 3. Create a function to handle the form submission
-   * 4. Separate the form from the component that displays the card upgrades
-   */
+	/**
+	 * TODO:
+	 * 1. Create a form with two inputs: commander and deck name
+	 * 2. Create a button to submit the form
+	 * 3. Create a function to handle the form submission
+	 * 4. Separate the form from the component that displays the card upgrades
+	 */
 
-  return (
-    <main>
-      <div className="max-w-lg flex-row bg-white shadow-md">
-        <form className="flex flex-row justify-center items-cente rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mx-5">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="commander"
-              type="text"
-              onChange={(e) => setCommander(e.target.value)}
-              placeholder="e.g Kardur, Doomscourge"
-            />
-          </div>
-          <div className="mx-5">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="deck-name"
-              type="text"
-              onChange={(e) => setDeckName(e.target.value)}
-              placeholder="e.g Chaos Incarnate"
-            />
-          </div>
-          <Button
-            text="Submit"
-            styles="cursor-pointer rounded-lg border bg-white px-3 tracking-wide h-10"
-            onClick={handleSubmit}
-          />
-        </form>
-        <div className="flex justify-center">
-          {isPending && <AiOutlineLoading3Quarters className="animate-spin" />}
-          {error && <div>{error}</div>}
-        </div>
-      </div>
-      <div className="flex w-full flex-row justify-evenly">
-        {data &&
-          data.map(({ title, cards }: ICardCategory, i: number) => {
-            return (
-              <div key={i}>
-                <h2 className="py-4 text-xl">{title}</h2>
-                <div>
-                  {cards.map(
-                    ({ name, synergy, inclusion }: ICard, i: number) => {
-                      return (
-                        <div key={i} style={{ fontSize: 14 }}>
-                          {name}
-                          {synergy ? `- Synergy: ${synergy}%` : ""}
-                          {inclusion ? ` - Inclusion: ${inclusion}` : ""}
-                        </div>
-                      );
-                    }
-                  )}
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="max-w-lg flex-row bg-white shadow-md">
+				<form className="flex flex-row justify-center items-cente rounded px-8 pt-6 pb-8 mb-4">
+					<div className="mx-5">
+						<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="commander"
+							type="text"
+							onChange={(e) => setCommander(e.target.value)}
+							placeholder="e.g Kardur, Doomscourge"
+						/>
+					</div>
+					<div className="mx-5">
+						<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="deck-name"
+							type="text"
+							onChange={(e) => setDeckName(e.target.value)}
+							placeholder="e.g Chaos Incarnate"
+						/>
+					</div>
+					<Button
+						text="Submit"
+						styles="cursor-pointer rounded-lg border bg-white px-3 tracking-wide h-10"
+						onClick={handleSubmit}
+					/>
+				</form>
+				<div className="flex justify-center">
+					{isPending && <div>Loading...</div>}
+					{error && <div>{error}</div>}
+				</div>
+			</div>
+			<div className="flex w-full flex-row justify-evenly">
+				{data &&
+					data.map(({ title, cards }: ICardCategory, i: number) => {
+						return (
+							<div key={i}>
+								<h2 className="py-4 text-xl">{title}</h2>
+								<div>
+									{cards.map(
+										({ name, synergy, inclusion }: ICard, i: number) => {
+											return (
+												<div key={i} style={{ fontSize: 14 }}>
+													{name}
+													{synergy ? `- Synergy: ${synergy}%` : ""}
+													{inclusion ? ` - Inclusion: ${inclusion}` : ""}
+												</div>
+											);
+										}
+									)}
+								</div>
+							</div>
+						);
+					})}
+			</div>
+		</main>
+	);
 }
